06d3d9919755c87a87ead13f0384c0e509d7785c
Rumit Shah <rumitshahn@gmail.com>
2024-03-13 22:32:26 +0530

    added comments and can now accept float values

--- Basic-Calculator.py
+++ Basic-Calculator.py
@@ -1,21 +1,24 @@
-def addition(n1, n2):
+def addition(n1, n2):  # Define a function for addition
     sum = n1 + n2
     return sum
 
-def subtraction(n1, n2):
+def subtraction(n1, n2):  # Define a function for subtraction
     sub = n1 - n2
     return sub
 
-def multiplication(n1, n2):
+def multiplication(n1, n2):  # Define a function for multiplication
     multiply = n1 * n2
     return multiply
 
-def division(n1, n2):
+def division(n1, n2):  # Define a function for division
     divide = n1/n2
     return divide
 
-operation = 0
+operation = 0  # Initialize variable for operation choice
+
+# Start a loop until the user chooses to end the program
 while operation != 5:
+    # Print menu for the user to choose an arithmetic operation
     print("""\nChoose any Arithmetic Operation you want to perform:
     (1)Addition
     (2) Subtraction
@@ -23,11 +26,14 @@ while operation != 5:
     (4) Division
     (5) END --- To close the calculator""")
 
+    # Ask user to enter operation choice
     operation = int(input("Enter the operation number: "))
 
-    n1 = int(input("Enter any number: "))
-    n2 = int(input("Enter any number: "))
+    # Ask user to enter two numbers
+    n1 = float(input("Enter first number: "))
+    n2 = float(input("Enter second number: "))
 
+    # Perform the chosen operation based on user input
     if operation == 1:
         print("\nThe Addition of numbers is ", addition(n1, n2))
 
@@ -41,8 +47,8 @@ while operation != 5:
         print("\nThe Division of numbers is ", division(n1, n2))
 
     elif operation == 5:
-        break
+        break  # Exit the loop if user chooses to end the program
 
     else:
-        print("\nEnter proper operation number!")
+        print("\nEnter proper operation number!")   # Error message for invalid operation choice
 
